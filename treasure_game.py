1a54b13bef7361f87076246af7cf5ce3057a820e
Kunal Guhagarkar <98008881+CoderKUN13@users.noreply.github.com>
2024-05-29 23:16:14 +0530

    Add files via upload

---
+++ treasure_game.py
@@ -0,0 +1,45 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+
+decison1 = input("You are on an island and you see a cave but there is a river in the way. Do you want to wait for a boat or swim accross? Choose 'swim' or 'wait' ").lower()
+
+if decison1 == "swim":
+  print("You swam accross the river and found three doors: 1.Red 2.Blue 3.Yellow")
+  decison2 = input("Which door do you want to pick? red, blue or yellow?").lower()
+  if decison2 == "red":
+    print("You enter a room of lions and you die....GAME OVER :(")
+  elif decison2 == "blue":
+    print("You enter a room of snakes and you die....GAME OVER :(")
+  elif decison2 == "yellow":
+    print("You enter a room of hot naked horny women.....YOU WIN!!!")
+else:
+  print("You wait and a Pirate boat arrives and kills you....GAME OVER :(")
+  
+
+
